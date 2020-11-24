129356147b44d35e799a32f4522fc2a5cecc22cf
Uthaiah BB <U.Bollerabopaiah@walmartlabs.com>
2020-11-24 18:51:45 +0530

    undo router path

--- src/js/router.config.js
+++ src/js/router.config.js
@@ -3,12 +3,12 @@ import List from "./views/help-list.js";
 
 export default [
   {
-    path: '/pulley-onboarding-design/',
+    path: '/',
     view: Home,
     defaultRoute: true
   },
   {
-    path: '/pulley-onboarding-design/list',
+    path: '/list',
     view: List
   }
 ];
