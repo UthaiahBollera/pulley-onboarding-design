9ff7a65472bf27089876050a4b25e0ad7184d707
Uthaiah BB <U.Bollerabopaiah@walmartlabs.com>
2020-11-24 18:44:38 +0530

    changed path

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import Router from "./lib/router.js";
-import routerConfig from "./router.config.js";
+import Router from "./src/js/lib/router.js";
+import routerConfig from "./src/js/router.config.js";
 const mountDoc = document.querySelector('#app');
 var router = new Router(routerConfig, mountDoc);
 export default {};
